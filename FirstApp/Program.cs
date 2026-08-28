9248383016300d34029242fcb00090105de2f0a5
Ibrahim Ahmed <i.a20011510@gmail.com>
2026-08-28 23:04:58 +0300

    q1

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace FirstApp
 {
-    internal class Program
+    internal class Book
     {
+        public string Title { get; private set; }
+        public int Pages { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //q1
+            //Book myBook = new Book();
+            //myBook.Title = "1984";
+            //myBook.Pages = 400;
+            //object bookObject = myBook; //declaration of obj type
+
+            //Console.WriteLine(bookObject);
         }
     }
 }
